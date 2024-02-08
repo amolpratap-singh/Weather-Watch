ba9f8716f9d9aed01a94e799b7fde59e1ee5cf4f
Amolpratap Singh <amolsingh.singh23@gmail.com>
2024-02-09 00:47:44 +0530

    opensearch function implemented

--- docker/weather-main-app/opensearch-db/opensearch_db.py
+++ docker/weather-main-app/opensearch-db/opensearch_db.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from elasticsearch import Elasticsearch
@@ -11,13 +12,30 @@ class OpenSearchDB(object):
                                 retry_on_timeout=True)
     
     def read_doc(self, index_name, doc_id):
-        pass
+        try:
+            self.logger.info(f"Reading {doc_id} from {index_name}")
+            doc = self.es.get(index=index_name, id=doc_id)
+            return doc
+        except NotFoundError as ex:
+            self.logger.error(f"Exception occurred during read operation on 
+                              Opensearch with {ex}")
+            return None
     
     def delete_doc(self, index_name, doc_id):
-        pass
+        self.logger.info(f"Deletion of id: {doc_id} from index: {index_name}")
+        result = self.es.delete(index=index_name, id=doc_id)
+        return result
+    
+    def create_doc(self, index_name, body, doc_id=None):
+        self.logger.info(f"Pushing Data : {body} to {index_name}")
+        if doc_id is None:
+            result = self.es.index(index=index_name, body=json.dumps(body))
+        else:
+            result = self.es.create(index=index_name, id=doc_id, body=json.dumps(body))
+        return result
     
     def update_doc(self, index_name, body):
         pass
     
-    def create_doc(self, index_name, body, key=None):
+    def create_index(self, index_name):
         pass
