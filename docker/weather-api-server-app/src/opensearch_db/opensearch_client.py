4dea2aa0ee043056b5f15400b69dfe27ecfe304e
Amolpratap Singh <amolsingh.singh23@gmail.com>
2024-05-15 00:14:00 +0530

    api server with docker compose

--- docker/weather-api-server-app/src/opensearch_db/opensearch_client.py
+++ docker/weather-api-server-app/src/opensearch_db/opensearch_client.py
@@ -1,28 +1,16 @@
 import os
-import json
-import logging
 import urllib3
-
 from opensearchpy import OpenSearch
-from opensearchpy.exceptions import NotFoundError
 
-#OPENSEARCH_HOST = os.getenv("OPENSEARCH_HOST","localhost")
-#OPENSEARCH_PORT = int(os.getenv("OPENSEARCH_PORT",9200))
+OPENSEARCH_HOST = os.getenv("OPENSEARCH_HOST","localhost")
+OPENSEARCH_PORT = int(os.getenv("OPENSEARCH_PORT",9200))
 
-OPENSEARCH_HOST = "localhost"
-OPENSEARCH_PORT = 9200
-
-#AUTH = ('admin', 'admin')
 AUTH = ('admin', 'weatherTest@123')
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# TODO Enable cert or TLS as well
 def get_opensearch_client():
-    es = OpenSearch(hosts=[{"hosts": OPENSEARCH_HOST, "port": OPENSEARCH_PORT}],
-                             http_auth=AUTH, use_ssl=True, verify_certs=False, 
-                             ssl_assert_hostname = False, ssl_show_warn = False)
-    
+    es = OpenSearch([f"https://{OPENSEARCH_HOST}:{OPENSEARCH_PORT}"], http_auth=AUTH, verify_certs=False)
     return es
     
 def close_opensearch_client(es_client):
