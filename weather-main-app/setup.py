b8abb0fb7f1f9a158daa83865afc5ee06383ec71
Amolpratap Singh <amolsingh.singh23@gmail.com>
2024-05-20 23:25:55 +0530

    creating packaging for weather-main-app

--- weather-main-app/setup.py
+++ weather-main-app/setup.py
@@ -3,12 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='weatherapp',
     version='1.0.0',
+    description="Weather Main App"
     packages=find_packages(),
-    #install_requires=[
+    author="Amolpratap Singh",
+    author_email="amolpratap.singh@yahoo.com",
+    install_requires=[
         # List your package dependencies here
-    #    'requests',
-        # Add any other dependencies required by your package
-    #],
+        'opensearch-py',
+        'python-dateutil',
+        'pytz',
+        'requests',
+        'schedule',
+        'urllib3'
+    ],
     #tests_require=[
     #    # List your test dependencies here
     #    'pytest',
